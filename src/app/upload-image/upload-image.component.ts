8f264424da0ea5a85c212485877dd9ac1a3659d4
aneeshgupta25 <aneeshgupta2512@gmail.com>
2024-10-06 14:37:23 +0530

    Output Events from Directive

--- src/app/upload-image/upload-image.component.ts
+++ src/app/upload-image/upload-image.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { UploadImageDirective } from './upload.image.directive';
 
 @Component({
   selector: 'app-upload-image',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, UploadImageDirective],
   templateUrl: './upload-image.component.html',
   styleUrl: './upload-image.component.css'
 })
-export class UploadImageComponent {
+export class UploadImageComponent implements AfterViewInit {
+
+  imageDropCardVisible: boolean = false;
+  @ViewChild('visible') visibleEl!: ElementRef;
+  @ViewChild('invisible') inVisibleEl!: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.inVisibleEl!.nativeElement.style.display = "none";
+    this.visibleEl!.nativeElement.style.display = "block";
+  }
+
+  ngOnInit() {
+  }
+
+  onImageDragOver() {
+    this.inVisibleEl.nativeElement.style.display = "block";
+    this.visibleEl.nativeElement.style.display = "none";
+  }
+
+  onImageDragLeave() {
+    this.inVisibleEl.nativeElement.style.display = "none";
+    this.visibleEl.nativeElement.style.display = "block";
+  }
 
 }
--- src/app/upload-image/upload.image.directive.ts
+++ src/app/upload-image/upload.image.directive.ts
@@ -1,9 +1,37 @@
-import { Directive } from "@angular/core";
+import { style } from "@angular/animations";
+import { Directive, HostBinding, HostListener, OnInit, output } from "@angular/core";
 
 @Directive({
     selector: '[appUploadImage]',
-    standalone: true
+    standalone: true,
 })
-export class UploadImageDirective {
-    
+export class UploadImageDirective implements OnInit {
+
+    @HostBinding('style.background-color') hostBackgroundColor!: string;
+    @HostListener('dragover', ["$event"]) dragOver(event: DragEvent) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.imageDragOver.emit();
+    }
+    @HostListener('dragleave', ["$event"]) dragLeave(event: DragEvent) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.imageDragLeave.emit();
+    }
+    @HostListener('drop', ["$event"]) drop(event: DragEvent) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+    imageDragOver = output();
+    imageDragLeave = output();
+
+    ngOnInit(): void {
+        // this.hostBackgroundColor = DropColor.BEFORE_DROP;
+    }
 }
+
+// enum DropColor {
+//     BEFORE_DROP = '#80808017',
+//     DURING_DROP = 'grey',
+//     AFTER_DROP = '#80808017'
+// }
